ede62e642ebf630d99ea904060bd98ad88dc9ee9
jaumecuadrada <jaumecuadrada.audio@gmail.com>
2023-01-31 23:47:31 +0100

    Implement main function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import Form from "./components/Form/Form";
+import "./css/styles.css";
+import getRandomGif from "./getRandomGif/getRandomGif";
+
 const App = (): JSX.Element => {
-  return <p></p>;
+  const [gifLink, setGifLink] = useState("");
+
+  const captureTag = async (tag: string) => {
+    const randomGif = await getRandomGif(tag);
+    setGifLink(randomGif);
+  };
+
+  return (
+    <div className="container">
+      <Form captureTag={captureTag} />
+      <div className="row">
+        <div className="container-gif offset-1 col-6">
+          <img src="" className="gif" alt="" />
+          <iframe
+            src={gifLink}
+            width="480"
+            height="270"
+            frameBorder="0"
+            className="giphy-embed"
+            allowFullScreen
+          ></iframe>
+          <p>
+            <a href="https://giphy.com/gifs/mlb-homer-donaldson-obxtPh0zkusBG">
+              via GIPHY
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <p className="error text-danger col">
+          Error: I couldn't retrieve anything funny
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default App;
